3831b18c2bc31f6175b16640a77eb1cad11b5820
Home <ilyakeps@yndex.ru>
2025-05-22 20:14:45 +0300

    add file

---
+++ index.py
@@ -0,0 +1,8 @@
+a = float(input("Сторона прямоугольника а: "))
+b = float(input("Сторона прямоугольника в: "))
+
+Ploshad = a * b
+Perimetr = (a + b) * 2
+
+print(f"Площадь равна:  {Ploshad}")
+print(f"Периметр равен: {Perimetr}")
